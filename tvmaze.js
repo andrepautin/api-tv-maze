e0c3dbc2912f3cf5ff3c4fa2fe3945645419b0d3
Kiru <kirushanth.ravindran@gmail.com>
2021-04-14 13:30:50 -0700

    get API to pull and return array of shows

--- tvmaze.js
+++ tvmaze.js
@@ -1,46 +1,53 @@
 "use strict";
-
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
-
-
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
 async function getShowsByTerm( /* term */) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  let userInput = $("#searchForm-term").val(); // gets the term that user wants to search
+  let searchesByTerm = await $.get(`http://api.tvmaze.com/search/shows?q=${userInput}`); // get all of the shows that match user's search
+  let shows = [];
+  for (let eachShow of searchesByTerm) { // create object for each show containing id, name, summary and image
+    // console.log(each);
+    let show = {
+      id: eachShow.show.id, 
+      name: eachShow.show.name, 
+      summary: eachShow.show.summary, 
+      image: eachShow.show.image};
+      // console.log(obj);
+    shows.push(show); // add object to array to return
+  }
+  // console.log(searchesByTerm);
+  // let firstShow = searchesByTerm[0].show;
+  // console.log(firstShow.id);
+  // console.log(firstShow.id, firstShow.name, firstShow.summary, firstShow.image.original);
+  return shows;
+    // {
+    //   id: 1767,
+    //   name: "The Bletchley Circle",
+    //   summary:
+    //     `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+    //        women with extraordinary skills that helped to end World War II.</p>
+    //      <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
+    //        normal lives, modestly setting aside the part they played in 
+    //        producing crucial intelligence, which helped the Allies to victory 
+    //        and shortened the war. When Susan discovers a hidden code behind an
+    //        unsolved murder she is met by skepticism from the police. She 
+    //        quickly realises she can only begin to crack the murders and bring
+    //        the culprit to justice with her former friends.</p>`,
+    //   image:
+    //       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    // }
 }
-
-
 /** Given list of shows, create markup for each and to DOM */
-
 function populateShows(shows) {
   $showsList.empty();
-
   for (let show of shows) {
     const $show = $(
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
@@ -59,35 +66,24 @@ function populateShows(shows) {
          </div>  
        </div>
       `);
-
     $showsList.append($show);  }
 }
-
-
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchForShowAndDisplay() {
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
-
   $episodesArea.hide();
   populateShows(shows);
 }
-
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
-
-
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
-
 // async function getEpisodesOfShow(id) { }
-
 /** Write a clear docstring for this function... */
-
 // function populateEpisodes(episodes) { }
